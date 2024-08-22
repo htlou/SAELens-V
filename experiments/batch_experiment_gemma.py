5429f67e7ca063ec68cca9182831e53f74c009b3
htlou <chenxinyu20040529@gmail.com>
2024-08-23 02:13:01 +0800

    upd code

--- experiments/batch_experiment_gemma.py
+++ experiments/batch_experiment_gemma.py
@@ -15,7 +15,7 @@ from transformer_lens import utils
 from functools import partial
 from jaxtyping import Int, Float
 
-device = ['cuda:1', 'cuda:2']
+device = ['cuda:0', 'cuda:1']
 
 
 from transformer_lens import HookedTransformer
@@ -50,35 +50,37 @@ import torch
 from transformers import AutoModelForCausalLM
 from transformer_lens import HookedTransformer
 from sae_lens import SAE
+import argparse
 
 tasks = {
+    "safety": {
+        "dataset": "/aifs4su/yaodong/projects/hantao/personal/data/safety_700.json",
+        "general": "/aifs4su/yaodong/projects/hantao/personal/data/base_700.json",
+        "model_path": "/aifs4su/yaodong/projects/hantao/personal/models/output_gemma_it_sft0716",
+    },
     "math": {
         "dataset": "/aifs4su/yaodong/projects/hantao/personal/data/MATH_700.json",
         "general": "/aifs4su/yaodong/projects/hantao/personal/data/base_700.json",
         "model_path": "/aifs4su/yaodong/projects/hantao/personal/models/0813/0814_output_gemma-2b-it_MATH_cor_sft",
-        "device": "cuda:2"
     },
     "reasoning": {
         "dataset": "/aifs4su/yaodong/projects/hantao/personal/data/reasoning_700.json",
         "general": "/aifs4su/yaodong/projects/hantao/personal/data/base_700.json",
         "model_path": "/aifs4su/yaodong/projects/hantao/personal/models/0813/0814_output_gemma-2b-it_reasoning_cor_sft",
-        "device": "cuda:2"
     },
     "empathy": {
         "dataset": "/aifs4su/yaodong/projects/hantao/personal/data/empathy_700.json",
         "general": "/aifs4su/yaodong/projects/hantao/personal/data/base_700.json",
         "model_path": "/aifs4su/yaodong/projects/hantao/personal/models/0813/0814_output_gemma-2b-it_empathy_cor_sft",
-        "device": "cuda:2"
     },
     "chinese": {
         "dataset": "/aifs4su/yaodong/projects/hantao/personal/data/zh_700.json",
         "general": "/aifs4su/yaodong/projects/hantao/personal/data/base_700.json",
-        "model_path": "/aifs4su/yaodong/projects/hantao/personal/models/0813/0814_output_gemma-2b-it_alpaca_zh_sft",
-        "device": "cuda:2"
+        "model_path": "/aifs4su/yaodong/projects/hantao/personal/models/0813/0814_output_gemma-2b-it_alpaca_zh_sft",\
     }
 }
 
-def task(data, template, model_0, sae_0, hook_point_0, model_1, sae_1, hook_point_1, topn=5):
+def task(data, template, model_0, sae_0, hook_point_0, model_1, sae_1, hook_point_1, topn):
     intersection_counts_full = torch.empty(0)
     value_changes_full = torch.empty(0)
     for piece in data:
@@ -188,7 +190,7 @@ def process_task(task_name, config):
     # Load model
     local_model = AutoModelForCausalLM.from_pretrained(config["model_path"])
     # get model
-    model = HookedTransformer.from_pretrained("gemma-2b-it", hf_model = local_model, device=config["device"])
+    model = HookedTransformer.from_pretrained("gemma-2b-it", hf_model = local_model, device=device[1])
 
     sae_1, _, _ = SAE.from_pretrained(
         release = "gemma-2b-it-res-jb",
@@ -204,10 +206,10 @@ def process_task(task_name, config):
     <start_of_turn>model
     {answer} """
 
-    intersection_mean, intersection_variance, value_changes_mean, value_changes_variance = task(task_data, template, model_0, sae_0, hook_point_0, model, sae_1, hook_point_1, topn=5)
+    intersection_mean, intersection_variance, value_changes_mean, value_changes_variance = task(task_data, template, model_0, sae_0, hook_point_0, model, sae_1, hook_point_1, config["topk"])
 
     # general data 
-    intersection_mean_general, intersection_variance_general, value_changes_mean_general, value_changes_variance_general = task(general_data, template, model_0, sae_0, hook_point_0, model, sae_1, hook_point_1, topn=5)
+    intersection_mean_general, intersection_variance_general, value_changes_mean_general, value_changes_variance_general = task(general_data, template, model_0, sae_0, hook_point_0, model, sae_1, hook_point_1, config["topk"])
     
     df_task = pd.DataFrame({
         "Task": [task_name, task_name],
@@ -220,18 +222,26 @@ def process_task(task_name, config):
 
     return df_task
 
-# Dictionary to store results from each task
-results = []
+def main():
+    parser = argparse.ArgumentParser(description='批量实验参数')
+    parser.add_argument('--topk', type=int, default=5, help='topk')
+    args = parser.parse_args()
 
-for task_name, config in tasks.items():
-    results.append(process_task(task_name, config))
+    # Dictionary to store results from each task
+    results = []
 
-# Concatenate all DataFrames and save to CSV
-final_df = pd.concat(results)
-final_df.to_csv("outputs/gemma_output.csv", index=False)
-print("Saved all task results to 'task_output.csv'")
+    for task_name, config in tasks.items():
+        config["topk"] = args.topk
+        results.append(process_task(task_name, config))
 
+    # Concatenate all DataFrames and save to CSV
+    file_name = f"outputs/gemma_output_top_{args.topk}.csv"
+    final_df = pd.concat(results)
+    final_df.to_csv(file_name, index=False)
+    print(f"Saved all task results to {file_name}")
 
+if __name__ == "__main__":
+    main()
 # Mean of Intersection Counts: tensor(3.3914)
 # Variance of Intersection Counts: tensor(0.8000)
 # Mean of Average Value Changes: tensor(0.0792)
--- experiments/batch_experiment_mistral.py
+++ experiments/batch_experiment_mistral.py
@@ -15,7 +15,7 @@ from transformer_lens import utils
 from functools import partial
 from jaxtyping import Int, Float
 
-device = ['cuda:1', 'cuda:2']
+device = ['cuda:2', 'cuda:3']
 
 
 from transformer_lens import HookedTransformer
@@ -50,35 +50,37 @@ import torch
 from transformers import AutoModelForCausalLM
 from transformer_lens import HookedTransformer
 from sae_lens import SAE
+import argparse
 
 tasks = {
+    "safety": {
+        "dataset": "/aifs4su/yaodong/projects/hantao/personal/data/safety_700.json",
+        "general": "/aifs4su/yaodong/projects/hantao/personal/data/base_700.json",
+        "model_path": "/aifs4su/yaodong/projects/hantao/personal/models/output_mistral_instruct_sft0801",
+    },
     "math": {
         "dataset": "/aifs4su/yaodong/projects/hantao/personal/data/MATH_700.json",
         "general": "/aifs4su/yaodong/projects/hantao/personal/data/base_700.json",
         "model_path": "/aifs4su/yaodong/projects/hantao/personal/models/0813/0814_output_Mistral-7B-Instruct-v0.1_MATH_cor_sft",
-        "device": "cuda:2"
     },
     "reasoning": {
         "dataset": "/aifs4su/yaodong/projects/hantao/personal/data/reasoning_700.json",
         "general": "/aifs4su/yaodong/projects/hantao/personal/data/base_700.json",
         "model_path": "/aifs4su/yaodong/projects/hantao/personal/models/0813/0814_output_Mistral-7B-Instruct-v0.1_reasoning_cor_sft",
-        "device": "cuda:2"
     },
     "empathy": {
         "dataset": "/aifs4su/yaodong/projects/hantao/personal/data/empathy_700.json",
         "general": "/aifs4su/yaodong/projects/hantao/personal/data/base_700.json",
         "model_path": "/aifs4su/yaodong/projects/hantao/personal/models/0813/0814_output_Mistral-7B-Instruct-v0.1_empathy_cor_sft",
-        "device": "cuda:2"
     },
     "chinese": {
         "dataset": "/aifs4su/yaodong/projects/hantao/personal/data/zh_700.json",
         "general": "/aifs4su/yaodong/projects/hantao/personal/data/base_700.json",
         "model_path": "/aifs4su/yaodong/projects/hantao/personal/models/0813/0814_output_Mistral-7B-Instruct-v0.1_alpaca_zh_sft",
-        "device": "cuda:2"
     }
 }
 
-def task(data, template, model_0, sae_0, hook_point_0, model_1, sae_1, hook_point_1):
+def task(data, template, model_0, sae_0, hook_point_0, model_1, sae_1, hook_point_1, topk):
     intersection_counts_full = torch.empty(0)
     value_changes_full = torch.empty(0)
     for piece in data:
@@ -103,7 +105,7 @@ def task(data, template, model_0, sae_0, hook_point_0, model_1, sae_1, hook_poin
         sae_out_0 = sae_0.decode(sv_feature_acts_0)
 
         # print out the top activations, focus on the indices
-        topk1 = torch.topk(sv_feature_acts_0, 5)
+        topk1 = torch.topk(sv_feature_acts_0, topk)
         # print(torch.topk(sv_feature_acts_0, 5))
 
 
@@ -118,7 +120,7 @@ def task(data, template, model_0, sae_0, hook_point_0, model_1, sae_1, hook_poin
         sae_out_1 = sae_1.decode(sv_feature_acts_1)
 
         # print out the top activations, focus on the indices
-        topk2 = torch.topk(sv_feature_acts_1, 5)
+        topk2 = torch.topk(sv_feature_acts_1, topk)
         # print(torch.topk(sv_feature_acts_1, 5))
 
         topk1_values_cpu = topk1.values.to('cpu')
@@ -190,7 +192,7 @@ def process_task(task_name, config):
     # Load model
     local_model = AutoModelForCausalLM.from_pretrained(config["model_path"])
     # get model
-    model = HookedTransformer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1", hf_model = local_model, device=config["device"])
+    model = HookedTransformer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1", hf_model = local_model, device=device[1])
 
     sae_1, _, _ = SAE.from_pretrained(
         release = "mistral-7b-res-wg",
@@ -204,10 +206,10 @@ def process_task(task_name, config):
     template = """<s>[INST] {prompt} [/INST]"
     {answer} """
 
-    intersection_mean, intersection_variance, value_changes_mean, value_changes_variance = task(task_data, template, model_0, sae_0, hook_point_0, model, sae_1, hook_point_1)
+    intersection_mean, intersection_variance, value_changes_mean, value_changes_variance = task(task_data, template, model_0, sae_0, hook_point_0, model, sae_1, hook_point_1, config["topk"])
 
     # general data 
-    intersection_mean_general, intersection_variance_general, value_changes_mean_general, value_changes_variance_general = task(general_data, template, model_0, sae_0, hook_point_0, model, sae_1, hook_point_1)
+    intersection_mean_general, intersection_variance_general, value_changes_mean_general, value_changes_variance_general = task(general_data, template, model_0, sae_0, hook_point_0, model, sae_1, hook_point_1, config["topk"])
     
     df_task = pd.DataFrame({
         "Task": [task_name, task_name],
@@ -220,17 +222,25 @@ def process_task(task_name, config):
 
     return df_task
 
-# Dictionary to store results from each task
-results = []
+def main():
+    parser = argparse.ArgumentParser(description='批量实验参数')
+    parser.add_argument('--topk', type=int, default=5, help='topk')
+    args = parser.parse_args()
+    # Dictionary to store results from each task
+    results = []
 
-for task_name, config in tasks.items():
-    results.append(process_task(task_name, config))
+    for task_name, config in tasks.items():
+        config["topk"] = args.topk
+        results.append(process_task(task_name, config))
 
-# Concatenate all DataFrames and save to CSV
-final_df = pd.concat(results)
-final_df.to_csv("outputs/mistral_output.csv", index=False)
-print("Saved all task results to 'task_output.csv'")
+    # Concatenate all DataFrames and save to CSV
+    final_df = pd.concat(results)
+    file_name = f"outputs/mistral_output_top_{args.topk}.csv"
+    final_df.to_csv(file_name, index=False)
+    print(f"Saved all task results to {file_name}")
 
+if __name__ == "__main__":
+    main()
 
 # Mean of Intersection Counts: tensor(3.3914)
 # Variance of Intersection Counts: tensor(0.8000)
