a48b06342acba5741deddc0b6b86bd36338773d6
antoinegg1 <antoine031106@gmail.com>
2024-11-22 22:13:11 +0800

    feature

--- tutorials/feature_data.py
+++ tutorials/feature_data.py
@@ -8,7 +8,7 @@ from sae_lens import SAE
 from transformer_lens.HookedLlava import HookedLlava
 from transformers import LlavaForConditionalGeneration, LlavaProcessor
 import tqdm
-
+from datasets import load_dataset 
 # 请确保以下导入的函数已正确实现
 from sae_lens.activation_visualization import (
     load_llava_model,
@@ -18,15 +18,17 @@ from sae_lens.activation_visualization import (
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Process some inputs.')
     parser.add_argument('--output_path', type=str, default='count_table.txt', help='保存结果的输出文件路径')
+    parser.add_argument('--model_name', type=str, default="llava-hf/llava-v1.6-mistral-7b-hf", help='模型名称')
     parser.add_argument('--model_path', type=str, required=True, help='模型路径')
     parser.add_argument('--sae_path', type=str, required=True, help='SAE模型路径')
     parser.add_argument('--sample_size', type=int, default=1000, help='采样的图像数量')
     parser.add_argument('--device', type=str, default='cuda:0', help='使用的设备，例如cuda:0')
-    parser.add_argument('--seed', type=int, default=42, help='随机种子')
+    parser.add_argument('--data_type', type=str, choices=['image', 'text'],
+                        default='image', help='数据类型：image或text')
     args = parser.parse_args()
     return args
 
-def prepare_batch_input(processor, device, file_paths, example_prompt):
+def prepare_batch_image_input(processor, device, file_paths, example_prompt):
     images = []
     prompts = []
     for file_path in file_paths:
@@ -51,23 +53,40 @@ def prepare_batch_input(processor, device, file_paths, example_prompt):
     ).to(device)
     return inputs
 
-def run_model(inputs, hook_language_model, sae, sae_device):
-    with torch.no_grad():
-        image_indices, cache = hook_language_model.run_with_cache(
-            input=inputs,
-            model_inputs=inputs,
-            vision=True,
-            prepend_bos=True,
-            names_filter=lambda name: name == sae.cfg.hook_name,
-            return_type="generate_with_saev",
-        )
+def prepare_batch_text_input(processor, device, texts):
+    inputs = processor(
+        text=texts,
+        return_tensors='pt',
+        padding=True,
+    ).to(device)
+    return inputs
 
+def run_model(inputs, hook_language_model, sae, sae_device, is_text=False):
+    with torch.no_grad():
+        if is_text:
+            cache = hook_language_model.run_with_cache(
+                input=inputs,
+                model_inputs=inputs,
+                vision=False,
+                prepend_bos=True,
+                names_filter=lambda name: name == sae.cfg.hook_name,
+                return_type="generate",
+            )
+        else:
+            image_indices, cache = hook_language_model.run_with_cache(
+                input=inputs,
+                model_inputs=inputs,
+                vision=True,
+                prepend_bos=True,
+                names_filter=lambda name: name == sae.cfg.hook_name,
+                return_type="generate_with_saev",
+            )
         tmp_cache = cache[sae.cfg.hook_name]
         tmp_cache = tmp_cache.to(sae_device)
         feature_acts = sae.encode(tmp_cache)
         sae_out = sae.decode(feature_acts)
         del cache
-    return image_indices, feature_acts
+    return feature_acts
 
 def save_results(output_path, feature_acts_count_table):
     with open(output_path, "w") as f:
@@ -84,9 +103,9 @@ def process_images(sampled_png_files, image_path, processor, device, hook_langua
         for i in range(0, num_files, batch_size):
             batch_files = sampled_png_files[i:i+batch_size]
             batch_file_paths = [os.path.join(image_path, f) for f in batch_files]
-            # 准备批量输入数据
+     
             inputs = prepare_batch_input(processor, device, batch_file_paths, example_prompt)
-            # 运行模型
+           
             _, feature_act = run_model(inputs, hook_language_model, sae, sae_device)
             # feature_act 的形状：[batch_size, sequence_length, feature_dim]
             feature_act = feature_act.cpu().detach().numpy()
@@ -102,11 +121,34 @@ def process_images(sampled_png_files, image_path, processor, device, hook_langua
             pbar.update(len(batch_files))
     return feature_acts_count_table
 
+def process_text(dataset, processor, device, hook_language_model, sae,
+                 sae_device, text_field, batch_size=32):
+    num_samples = len(dataset)
+    feature_acts_count_table = np.zeros(65536, dtype=int)
+    print("开始处理文本数据...")
+
+    with tqdm.tqdm(total=num_samples) as pbar:
+        for i in range(0, num_samples, batch_size):
+            batch = dataset[i:i+batch_size]
+            texts = batch[text_field]
+            inputs = prepare_batch_text_input(processor, device, texts)
+            feature_act = run_model(inputs, hook_language_model, sae,
+                                    sae_device, is_text=True)
+            feature_act = feature_act.cpu().detach().numpy()
+            last_token_feature_act = feature_act[:, -1, :]
+            indices = np.where(last_token_feature_act > 1)
+            feature_indices = indices[1]
+            unique_features, counts = np.unique(feature_indices,
+                                                return_counts=True)
+            feature_acts_count_table[unique_features] += counts
+            pbar.update(len(texts))
+    return feature_acts_count_table
+
 def main():
     args = parse_arguments()
 
     # 设置随机种子
-    random_seed = args.seed
+    random_seed =42
     random.seed(random_seed)
 
     # 设置设备
@@ -114,34 +156,43 @@ def main():
     sae_device = args.device  # 将所有模型和数据放在同一设备上
 
     # 设置其他参数
-    MODEL_NAME = "llava-hf/llava-v1.6-mistral-7b-hf"
+    model_name = args.model_name
     model_path = args.model_path
     sae_path = args.sae_path
-    example_prompt = "The color in this image is"
-    image_path = "/home/saev/changye/data/colors/color_images"
     output_path = args.output_path
     sample_size = args.sample_size
 
     # 加载模型
     processor, vision_model, vision_tower, multi_modal_projector, hook_language_model = load_llava_model(
-        MODEL_NAME, model_path, device
+        model_name, model_path, device
     )
     sae = load_sae(sae_path, sae_device)
 
-    # 获取图像文件列表
-    files = os.listdir(image_path)
-    png_files = [f for f in files if f.lower().endswith('.png')]
-    png_files.sort()
-
-    # 随机采样
-    if sample_size > len(png_files):
-        sample_size = len(png_files)
-    sampled_png_files = random.sample(png_files, sample_size)
-
-    # 处理图像并获取结果
-    feature_acts_count_table = process_images(
-        sampled_png_files, image_path, processor, device, hook_language_model, sae, sae_device, example_prompt
-    )
+    if args.data_type == 'image':
+        example_prompt = "The color in this image is"
+        image_path = "/home/saev/changye/data/colors/color_images"
+        files = os.listdir(image_path)
+        png_files = [f for f in files if f.lower().endswith('.png')]
+        png_files.sort()
+        if sample_size > len(png_files):
+            sample_size = len(png_files)
+        sampled_png_files = random.sample(png_files, sample_size)
+        feature_acts_count_table = process_images(
+            sampled_png_files, image_path, processor, device,
+            hook_language_model, sae, sae_device, example_prompt)
+    elif args.data_type == 'text':
+        if args.dataset_name is None:
+            raise ValueError("数据类型为文本时，需要指定dataset_name参数")
+        dataset = load_dataset(args.dataset_name, split=args.split)
+        if sample_size > len(dataset):
+            sample_size = len(dataset)
+        dataset = dataset.shuffle(seed=random_seed)
+        dataset = dataset.select(range(sample_size))
+        feature_acts_count_table = process_text(
+            dataset, processor, device, hook_language_model, sae,
+            sae_device, args.text_field)
+    else:
+        raise ValueError("未知的数据类型：{}".format(args.data_type))
 
     # 保存结果
     save_results(output_path, feature_acts_count_table)
