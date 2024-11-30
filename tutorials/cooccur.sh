40606b0ed5ca9c9ef0b440fdc1c1f4891604c865
antoinegg1 <antoine031106@gmail.com>
2024-11-30 19:14:32 +0800

    coocur5

--- tutorials/cooccur.sh
+++ tutorials/cooccur.sh
@@ -1,6 +1,12 @@
-# python /data/changye/SAELens-V/tutorials/cooccurrence.py --start_idx 13000 --end_idx 21000
-# python /data/changye/SAELens-V/tutorials/cooccurrence.py --start_idx 21000 --end_idx 29000
-# python /data/changye/SAELens-V/tutorials/cooccurrence.py --start_idx 29000 --end_idx 37000
+python /data/changye/SAELens-V/tutorials/cooccurrence.py --start_idx 0 --end_idx 6000
+python /data/changye/SAELens-V/tutorials/cooccurrence.py --start_idx 6000 --end_idx 13000
+python /data/changye/SAELens-V/tutorials/cooccurrence.py --start_idx 13000 --end_idx 21000
+python /data/changye/SAELens-V/tutorials/cooccurrence.py --start_idx 21000 --end_idx 29000
+python /data/changye/SAELens-V/tutorials/cooccurrence.py --start_idx 29000 --end_idx 37000
 python /data/changye/SAELens-V/tutorials/cooccurrence.py --start_idx 37000 --end_idx 45000
 python /data/changye/SAELens-V/tutorials/cooccurrence.py --start_idx 45000 --end_idx 53000
 python /data/changye/SAELens-V/tutorials/cooccurrence.py --start_idx 53000 --end_idx 61000
+python /data/changye/SAELens-V/tutorials/cooccurrence.py --start_idx 61000 --end_idx 69000
+python /data/changye/SAELens-V/tutorials/cooccurrence.py --start_idx 69000 --end_idx 78000
+python /data/changye/SAELens-V/tutorials/cooccurrence.py --start_idx 78000 --end_idx 87000
+python /data/changye/SAELens-V/tutorials/cooccurrence.py --start_idx 87000 --end_idx 96000
